72725329fb06732833c57e20b5623398eeb43b9b
boring-km <kms0644804@naver.com>
2022-08-09 23:35:40 +0900

    score 저장 기능 구현

--- lib/presentation/game/game_over_controller.dart
+++ lib/presentation/game/game_over_controller.dart
@@ -1,6 +1,9 @@
 import 'package:dodge_game/game/game.dart';
 import 'package:dodge_game/presentation/game/game_timer_controller.dart';
 import 'package:get/get.dart';
+import 'package:intl/intl.dart';
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
 
 class GameOverController extends GetxController {
   bool isGameOver = false;
@@ -8,6 +11,7 @@ class GameOverController extends GetxController {
   final GameTimerController _timerController = Get.find<GameTimerController>();
 
   void gameOver(String timeText) {
+    _saveScore(timeText.replaceAll('s', ''));
     isGameOver = true;
     _timeText = timeText;
     update();
@@ -22,5 +26,42 @@ class GameOverController extends GetxController {
     _timerController.restart();
     isGameOver = false;
     update();
+  }
+
+  Future<void> _saveScore(String time) async {
+    final db = await _getDb();
+    final now = DateTime.now();
+    final formattedDate = DateFormat('yyyy-MM-dd hh:mm:ss').format(now);
+
+    await db.transaction((txn) async {
+      await txn.rawInsert(
+        'INSERT INTO Score(num, time) VALUES($time, "$formattedDate")',
+      );
+    });
+
+    final list = await db.rawQuery('SELECT * FROM Score');
+    for (final item in list) {
+      print(item);
+    }
+  }
+
+  Future<Database> _getDb() async {
+    final path = join(await getDatabasesPath(), 'score.db');
+    final db = await openDatabase(
+      path,
+      version: 1,
+      onCreate: (db, version) {
+        db.execute(
+          '''
+          CREATE TABLE Score(
+            id INTEGER PRIMARY KEY,
+            num INTEGER,
+            time VARCHAR(30) 
+          );
+        ''',
+        );
+      },
+    );
+    return db;
   }
 }
